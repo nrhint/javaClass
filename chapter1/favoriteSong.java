0c0d09ec1f154c4c645e30415e034a45ddd02893
nrhint <nathan@hintonclan.org>
2017-01-09 21:55:59 +0000

    -commit- 1-9-2016

---
+++ chapter1/favoriteSong.java
@@ -0,0 +1,15 @@
+/////////////////
+//Nathan Hinton//
+//Dec-22-2016  //
+/////////////////
+
+public class favoriteSong
+{
+    public static void main(String[] args)
+    {
+	System.out.println("Give said the little stream.");
+	System.out.println("Give oh Give, Give oh Give.");
+	System.out.println("Give said the little stream.");
+	System.out.println("As it huryed down the hill.");
+    }
+}
---
+++ chapter1/tableAndChairs.java
@@ -0,0 +1,18 @@
+/////////////////
+//Nathan Hinton//
+//Dec-22-2016  //
+/////////////////
+
+public class tableAndChairs
+{
+    public static void main(String[] args)
+    {
+	System.out.println("x                    x");
+	System.out.println("x                    x");
+	System.out.println("x     xxxxxxxxxx     x");
+	System.out.println("xxxxx x        x xxxxx");
+	System.out.println("x   x x        x x   x");
+	System.out.println("x   x x        x x   x");
+	System.out.println("xxxxxxxxxxxxxxxxxxxxxx");
+    }
+}
