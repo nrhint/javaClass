2d739a584e361be27f96845b9614cd796ca43623
nrhint <nathan@hintonclan.org>
2017-01-09 22:51:05 +0000

    I am trying to finish two chapters this week.  Start date is: Jan-9-2017.  End date is: Jan-14-2017.

---
+++ chapter2/DataDemo.java
@@ -0,0 +1,12 @@
+public class DataDemo
+{
+    public static void main(String[] vars)
+    {
+	int myNumber = 15;
+	final int STATES = 50;
+	System.out.print("Nathans favorite number is: ");
+	System.out.println(myNumber);
+	System.out.print("The number of states is " + STATES);
+	System.out.println();
+    }
+}
---
+++ chapter2/vars1.java
@@ -0,0 +1,12 @@
+public class vars1
+{
+    public static void main(String[] args)
+    {
+	int num =  26;
+	System.out.print("Nathans birthday is on the ");
+	System.out.print(num);
+	System.out.println(" of the month");
+	System.out.println("Next month: December");
+	System.out.println();
+    }
+}
