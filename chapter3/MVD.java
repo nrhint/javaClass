68f361755a0426c87a13c269e48c73295a4d98f2
nrhint <nathan@hintonclan.org>
2017-03-17 02:21:13 +0000

    chapter 3

--- chapter3/MVD.java
+++ chapter3/MVD.java
@@ -1,6 +1,12 @@
 public class MVD
 {
-    public static void main(string[] args)
+    public static void main(String[] args)
     {
+	System.out.println("Calling data from another class:");
+	displayInfo();
+    }
+    public static void displayInfo()
+    {
+	System.out.println("We will design and make circuit boards and test them for you!");
     }
 }
